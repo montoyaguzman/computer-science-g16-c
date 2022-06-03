de0d84b9674e9a194da8dc2eb285e971115bbd83
j.montoya <j.montoya@bbva.com>
2022-06-03 00:07:01 -0500

    feat: codigo del factorial

--- 1-logica-programacion/1.4-code/factorial.js
+++ 1-logica-programacion/1.4-code/factorial.js
@@ -2,14 +2,10 @@ var numero = window.prompt("¿De que cvalor necesitas el factorial?");
 numero = parseInt(numero);
 var factorial = 1;
 
-if (numero === 0 || numero === 1) {
-  factorial = 1;
-} else {
-  console.log('use while');
-  while (numero != 0) {
+while (numero != 0 && numero !== 1) {
+    console.log('use el while');
     factorial = factorial * numero;
-  	numero--;
-  } 
+    numero--;
 }
 
 console.log("El resultado del factorial es: ", factorial);
