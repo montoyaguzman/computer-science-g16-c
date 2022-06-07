8b9ec0e2bfeb9c3f3414422937c8c816ea353532
montoyaguzman <montoyaguzman7@gmail.com>
2022-06-06 20:05:17 -0500

    feat: empezando ejercicio de matrices

---
+++ 1-logica-programacion/1.4-code/matrices/matrices.js
@@ -0,0 +1,39 @@
+var matrizA = [
+    [2, 4, 1],
+    [3, 9, 3],
+    [5, 8, 1],
+];
+
+var matrizB = [
+    [1, 0, 1],
+    [1, 2, 1],
+    [1, 1, 0],
+];
+
+var matrizC = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
+
+
+for (var i = 0; i < matrizA.length; i++) {
+  for (var j = 0; j < matrizA.length; j++) {
+    console.log("matrizA: ", matrizA[i][j]);
+    // i=0, j=0, j (0) < matrizA.length (3) true
+    // i=0, j=1, j (1) < matrizA.length (3) true 
+    // i=0, j=2, j (2) < matrizA.length (3) true
+    // i=0, j=3, j (3)< matrizA.length (3) false
+    
+    // i=1, j=0, j (0) < matrizA.length (3) true
+    // i=1, j=1, j (1) < matrizA.length (3) true 
+    // i=1, j=2, j (2) < matrizA.length (3) true
+    // i=1, j=3, j (3)< matrizA.length (3) false
+    
+    // i=2, j=0, j (0) < matrizA.length (3) true
+    // i=2, j=1, j (1) < matrizA.length (3) true 
+    // i=2, j=2, j (2) < matrizA.length (3) true
+    // i=2, j=3, j (3)< matrizA.length (3) false
+  }
+  // i++
+}
