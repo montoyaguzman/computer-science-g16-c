9cd5469a62a2ea044aa6aef27e7ba55919526984
montoyaguzman <montoyaguzman7@gmail.com>
2022-06-08 21:12:16 -0500

    feat: notas adicionales para rest operator

--- 2-ecmascript/6-default-parameters.js
+++ 2-ecmascript/6-default-parameters.js
@@ -2,5 +2,6 @@ function suma(numero1 = 0, numero2 = 0) {
     let resultado = numero1 + numero2;
     return resultado;
 }
-const resultado = suma(3);
+const resultado = suma(3); // 3 + 0
+// const resultado = suma(undefined, 13); // 0 +  13
 console.log('resultado: ', resultado);
