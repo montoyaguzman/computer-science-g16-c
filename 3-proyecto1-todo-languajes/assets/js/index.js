8dbb703a04c8f107563b0e172c0ddd2c4803adcb
montoyaguzman <montoyaguzman7@gmail.com>
2022-06-13 20:05:28 -0500

    feat: avance del proyecto todo

--- 3-proyecto1-todo-languajes/assets/js/index.js
+++ 3-proyecto1-todo-languajes/assets/js/index.js
@@ -1,1 +1,18 @@
-console.log('JS loaded succesfully!')
+// const ulElement = document.getElementById('list-languages');
+// getElementById
+// getElementByClassName
+// getElementByTagName
+const ulElement2 = document.querySelector('#list-languages');
+// id => #
+// class => .
+// tag => nameTag
+
+const renderElementList = () => {
+    // creación de los elementos HTML
+    const liElement = document.createElement('li');
+    const iElement = document.createElement('i');
+    const divElement = document.createElement('div');
+    const buttonElement = document.createElement('button');
+};
+
+
