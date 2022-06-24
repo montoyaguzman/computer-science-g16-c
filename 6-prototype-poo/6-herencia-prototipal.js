12267dd7b5a5a4ce5db781c55649d001a53ce05b
montoyaguzman <montoyaguzman7@gmail.com>
2022-06-24 10:56:05 -0500

    feat: herencia en funciones constructoras

--- 6-prototype-poo/6-herencia-prototipal.js
+++ 6-prototype-poo/6-herencia-prototipal.js
@@ -13,35 +13,34 @@ createPersona.prototype.comer = () => {
     console.log('Soy metodo de persona y puedo comer');
 };
 
-function createPanadero2(name, altura, colorPiel, isComerciante, hasMandil) {
-    this.base = new createPersona(name, altura, colorPiel);
+function createPanadero(name, altura, colorPiel, isComerciante, hasMandil) {
+    this.__proto__ = new createPersona(name, altura, colorPiel);
     this.isComerciante = isComerciante;
     this.hasMandil = hasMandil;
     Object.seal(this);
 };
-createPanadero2.prototype = new createPersona;
 
 createPanadero.prototype.cocina = () => {
     console.log('El panadero con el pan :v');
 };
 
-function createSuperHumano(superpoder, tipo, alias) {
+function createSuperHumano(name, altura, colorPiel, superpoder, tipo, alias) {
+    this.__proto__ = new createPersona(name, altura, colorPiel);
     this.superpoder = superpoder;
     this.tipo = tipo;
     this.alias = alias;
-    this.__proto__ = createPersona
-    Object.seal(this);
 };
 
 createSuperHumano.prototype.volar = () => {
     console.log('Soy superhumano y puedo volar');
 };
 
-const flash = new createSuperHumano("super velocidad", "super heroe", "flash");
+// const flash = new createSuperHumano("super velocidad", "super heroe", "flash");
+const flash = new createSuperHumano("barry", 1.78, "clara", "super velocidad", "super heroe", "flash");
 console.log(flash);
 console.log(flash.volar());
 
 const personaGenerica = new createPersona("robot", 1.78, 'morena');
 // const tinTanElPanadero = new createPanadero(true, true);
-const tinTanElPanadero = new createPanadero2("tintan", 1.78, 'morena', true, true);
+const tinTanElPanadero = new createPanadero("tintan", 1.78, 'morena', true, true);
 
